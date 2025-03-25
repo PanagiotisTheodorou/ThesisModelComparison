cbf6fea05cfd92799ac28aa3c385f89f57075dd2
PanagiotisTheodorou <PTheodorou@uclan.ac.uk>
2025-03-25 11:31:40 +0200

    Updated Data Analysis Script 3 - Show value distribution

--- DataAnalysis/003_MinMaxColumns.py
+++ DataAnalysis/003_MinMaxColumns.py
@@ -2,8 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+"""
+    The code performs the following:
+        1. Loads the dataset
+        2. Selects te numerical columns
+        3. calculates the min and max values 
+        4. Shows the results in a box plot
+"""
+
 # Load the dataset
-df = pd.read_csv('ThyroxineData_Cleaned_NoOutliers.csv')
+df = pd.read_csv('../data/ThyroxineData_Cleaned_NoOutliers.csv')
 
 # Select only numeric columns
 numeric_columns = df.select_dtypes(include=['number'])
@@ -14,9 +22,9 @@ summary_stats = numeric_columns.agg(['min', 'mean', 'max'])
 # Print the results
 print(summary_stats)
 
-# Plot boxplots for outlier detection
+# Plot box-plots for outlier detection
 plt.figure(figsize=(12, 6))
 sns.boxplot(data=numeric_columns)
 plt.xticks(rotation=90)
-plt.title("Boxplots for Numeric Columns (Outlier Detection)")
+plt.title("Box-plots for Numeric Columns (Outlier Detection)")
 plt.show()
