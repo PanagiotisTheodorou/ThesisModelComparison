945114b5ea747cbb814a27776c2ac0447f5afb52
PanagiotisTheodorou <PTheodorou@uclan.ac.uk>
2025-03-25 11:41:28 +0200

    Updated Data Analysis Script 4 - Fix Age column outliers

--- DataAnalysis/004_RemoveOutliers.py
+++ DataAnalysis/004_RemoveOutliers.py
@@ -1,7 +1,19 @@
 import pandas as pd
 
+"""
+    The code performs the following: 
+        1. Loads the dataset
+        2. Calculate the 1st and 3rd quantiles for the age column (saw in the previous script that there are outliers)
+        3. Calculate the difference between the 1st and 3rd quantile ranges (IQR)
+        4. Uses the IQR to find outliers 
+        5. Filters the outliers
+        6. Saves a new Dataset without outliers
+"""
+
+# A quantile is a dividing point, that sets apart the distribution to sectors with similar probabilities
+
 # Load dataset
-df = pd.read_csv('ThyroxineData_Cleaned_v2.csv')
+df = pd.read_csv('../data/ThyroxineData_Cleaned_v2.csv')
 
 # Compute Q1 (25th percentile) and Q3 (75th percentile) for the Age column
 Q1 = df['age'].quantile(0.25)
@@ -18,7 +30,7 @@ upper_bound = Q3 + 1.5 * IQR
 df_filtered = df[(df['age'] >= lower_bound) & (df['age'] <= upper_bound)]
 
 # Save the cleaned dataset
-df_filtered.to_csv('ThyroxineData_Cleaned_NoOutliers.csv', index=False)
+df_filtered.to_csv('../data/ThyroxineData_Cleaned_NoOutliers.csv', index=False)
 
 print(f"Original dataset size: {df.shape[0]} rows")
 print(f"Filtered dataset size: {df_filtered.shape[0]} rows")
