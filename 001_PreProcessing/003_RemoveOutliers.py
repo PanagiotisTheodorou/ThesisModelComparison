ea6fa2dfc69aacd06570cbf39796d908e0c2af86
PanagiotisTheodorou <PTheodorou@uclan.ac.uk>
2025-03-28 18:57:07 +0200

    Changed Comments on 003 remove outliers

--- 001_PreProcessing/003_RemoveOutliers.py
+++ 001_PreProcessing/003_RemoveOutliers.py
@@ -10,16 +10,13 @@ import pandas as pd
         6. Saves a new Dataset without outliers
 """
 
-# A quantile is a dividing point, that sets apart the distribution to sectors with similar probabilities
-
-# Load dataset
 df = pd.read_csv('../000_Data/002_dataset_after_filling_missing_values.csv')
 
-# Compute Q1 (25th percentile) and Q3 (75th percentile) for the Age column
+# Compute Q1  and Q3
 Q1 = df['age'].quantile(0.25)
 Q3 = df['age'].quantile(0.75)
 
-# Compute IQR (Interquartile Range)
+# Compute IQR
 IQR = Q3 - Q1
 
 # Define lower and upper bounds for outliers
@@ -29,7 +26,6 @@ upper_bound = Q3 + 1.5 * IQR
 # Filter out outliers
 df_filtered = df[(df['age'] >= lower_bound) & (df['age'] <= upper_bound)]
 
-# Save the cleaned dataset
 df_filtered.to_csv('../000_Data/003_dataset_after_removing_outliers.csv', index=False)
 
 print(f"Original dataset size: {df.shape[0]} rows")
